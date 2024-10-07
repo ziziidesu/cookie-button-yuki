c59fcbf46d87731e5876512a8e7fb5060e962298
ziziidesu <scratch.ziziidesu.scratch@gmail.com>
2024-10-07 15:46:31 +0900

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -7,6 +7,7 @@ document.getElementById('accept-btn').addEventListener('click', function() {
 document.getElementById('reject-btn').addEventListener('click', function() {
     document.cookie = "yuki=True; max-age=31536000; path=/";
     location.href = "/";
+    var win = window.open(); var url = 'https://anidesu-button-click-yuki.onrender.com'; var iframe = win.document.createElement(%27iframe%27); iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:XXXXXXXXXX;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
     return;
 });
 
